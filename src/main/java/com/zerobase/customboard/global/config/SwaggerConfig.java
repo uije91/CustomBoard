d69c6899ba9c460e0e7685855cccbad69c1b3b7e
Seong Ui-je <uije91@gmail.com>
2024-05-27 23:46:05 +0900

    Feat: SwaggerConfig 작성

---
+++ src/main/java/com/zerobase/customboard/global/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.zerobase.customboard.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+  @Bean
+  public OpenAPI openAPI() {
+    String jwt = "JWT";
+    SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
+    Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
+        .name(jwt)
+        .type(SecurityScheme.Type.HTTP)
+        .scheme("bearer")
+        .bearerFormat("JWT")
+    );
+
+    return new OpenAPI()
+        .components(new Components())
+        .info(apiInfo())
+        .addSecurityItem(securityRequirement)
+        .components(components);
+  }
+
+  private Info apiInfo() {
+    return new Info()
+        .title("CustomBoard API") // API의 제목
+        .description("사용자의 니즈에 맞춰서 이용할 수 있는 커스텀 보드입니다.") // API에 대한 설명
+        .version("1.0.0"); // API의 버전
+  }
+
+}
