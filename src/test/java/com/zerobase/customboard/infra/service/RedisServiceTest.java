6d4f0f6f39641d5b093219f26055677c5fe63f65
Seong Ui-je <uije91@gmail.com>
2024-05-28 17:18:46 +0900

    Test: Redis 테스트

---
+++ src/test/java/com/zerobase/customboard/infra/service/RedisServiceTest.java
@@ -0,0 +1,95 @@
+package com.zerobase.customboard.infra.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+@ExtendWith(MockitoExtension.class)
+class RedisServiceTest {
+
+  @InjectMocks
+  private RedisService redisService;
+
+  @Mock
+  private RedisTemplate<String, Object> redisTemplate;
+
+  @Mock
+  private ValueOperations<String, Object> valueOperations;
+
+  @Test
+  @DisplayName("Redis 데이터 가져오기")
+  void testGetData() throws Exception{
+    // given
+    String key = "testKey";
+    String expectedValue = "testValue";
+
+    when(redisTemplate.opsForValue()).thenReturn(valueOperations);
+    when(valueOperations.get(key)).thenReturn(expectedValue);
+
+    // when
+    String actualValue = redisService.getData(key);
+
+    //then
+    assertEquals(expectedValue, actualValue);
+    verify(redisTemplate.opsForValue(), times(1)).get(key);
+  }
+
+  @Test
+  @DisplayName("Redis 데이터 삭제")
+  void testDeleteData() throws Exception{
+    // given
+    String key = "testKey";
+
+    // when
+    redisService.deleteData(key);
+
+    //then
+    verify(redisTemplate, times(1)).delete(key);
+  }
+
+  @Test
+  @DisplayName("Redis 데이터 저장")
+  void testSetDataExpire() throws Exception{
+    // given
+    String key = "testKey";
+    String value = "testValue";
+    long expired = 1000L;
+
+    when(redisTemplate.opsForValue()).thenReturn(valueOperations);
+
+    // when
+    redisService.setDataExpire(key,value,expired);
+
+    //then
+    verify(valueOperations, times(1)).set(key, value, expired, TimeUnit.MILLISECONDS);
+  }
+
+  @Test
+  @DisplayName("Redis 데이터 존재여부 확인")
+  void testExistData() throws Exception{
+    // given
+    String key = "testKey";
+
+    when(redisTemplate.hasKey(key)).thenReturn(true);
+
+    // when
+    boolean exists = redisService.existData(key);
+
+    //then
+    assertTrue(exists);
+    verify(redisTemplate, times(1)).hasKey(key);
+  }
+
+}
